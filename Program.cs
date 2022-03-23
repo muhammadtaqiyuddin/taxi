ccb5cd51c22a29a4790ba56b8e6b6241ff9dff9b
Taqi <mtaqiyuddin101@gmail.com>
2022-03-23 16:59:53 +0700

    taxi

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LatihanTaxi;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        // membuat objek Taxi
+        Taxi taxi = new Taxi();
+
+        // pengesetan nilai properties
+        taxi.DriverName = "Jono";
+        taxi.OnDuty = true;
+        taxi.NumPassenger = 10;
+
+        // Pemanggilan method
+        taxi.TaxiInfo();
+        taxi.PickUpPassenger();
+        taxi.DropOffPassenger();
+
+        Console.ReadKey();
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanTaxi
+{
+    public class Taxi
+    {
+        // properties
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+        
+            //method
+        public void TaxiInfo()
+        {
+            
+            Console.WriteLine("Driver Name: {0}", DriverName);
+            if (OnDuty == true)
+            {  
+                Console.WriteLine("On Duty: Yes");
+            }
+            else if(OnDuty == false)
+            { 
+                Console.WriteLine("On Duty: No");
+            }
+            Console.WriteLine("Number Of Passenger: {0}", NumPassenger);
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("\n{0} sedang menjemput penumpang", DriverName);
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang", DriverName);
+        }
+
+    }
+
+
+}
